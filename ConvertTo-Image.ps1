ec91abadcd1cd20e4ec2b9d3854be62e5cebd91d
secretGeek <leonbambrick@gmail.com>
2016-06-19 00:31:15 +1000

    added autosizing to ConvertTo-Image

--- ConvertTo-Image.ps1
+++ ConvertTo-Image.ps1
@@ -13,14 +13,24 @@ function ConvertTo-Image
         foreach ($object in $InputObject) 
         { 
 
-        # From http://stackoverflow.com/questions/2067920/can-i-draw-create-an-image-with-a-given-text-with-powershell
-        
+            # From http://stackoverflow.com/questions/2067920/can-i-draw-create-an-image-with-a-given-text-with-powershell
             $filename1 = (Join-Path $psScriptRoot (([guid]::NewGuid().ToString())+".png"))
-            $bmp1 = new-object System.Drawing.Bitmap 250,61 
+            #TODO: Configure font name
+            #TODO: Configure font size
             $font = new-object System.Drawing.Font Consolas,24 
+            $textSize = [System.Windows.Forms.TextRenderer]::MeasureText($object, $font)
+            # the size of the image is based on the measure size of the text.
+            $bmp1 = new-object System.Drawing.Bitmap ($textSize.Width+20),($textSize.Height+20)
+            #TODO: Configure background color (including transparency)
             $brushBg = [System.Drawing.Brushes]::Yellow 
+            #TODO: Configure foreground color (including transparency)
             $brushFg = [System.Drawing.Brushes]::Black 
+            #TODO: stretch goal... allow for text outline ('stroke')
+            #TODO: stretch goal... allow for background to be an image
             $graphics = [System.Drawing.Graphics]::FromImage($bmp1) 
+            $graphics.CompositingQuality = [System.Drawing.Drawing2D.CompositingQuality]::HighQuality
+            $graphics.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+            $graphics.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::AntiAlias
             $graphics.FillRectangle($brushBg,0,0,$bmp1.Width,$bmp1.Height) 
             $graphics.DrawString($object,$font,$brushFg,10,10) 
             $graphics.Dispose() 
