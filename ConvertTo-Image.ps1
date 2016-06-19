fcffb98dc40d2a52ffd99f0ddf211db0ef97d1e9
secretGeek <leonbambrick@gmail.com>
2016-06-19 11:56:38 +1000

    Added parameters to ConvertTo-Image

--- ConvertTo-Image.ps1
+++ ConvertTo-Image.ps1
@@ -2,7 +2,18 @@
 function ConvertTo-Image
 {
     [CmdletBinding()] 
-    param([Parameter(Position=0, Mandatory=$true, ValueFromPipeline = $true)] [PSObject[]]$InputObject) 
+    param(
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline = $true)] [PSObject[]]$InputObject,
+        
+        [parameter(Mandatory=$false)][alias("F")][string]$FontName = "Consolas",
+        [parameter(Mandatory=$false)][alias("BG")][string]$BackgroundColor = "Black",
+        [parameter(Mandatory=$false)][alias("FG")][string]$ForegroundColor = "Green",
+        [parameter(Mandatory=$false)][alias("FS")][int]$FontSize = 24,
+        [parameter(Mandatory=$false)][alias("ML")][int]$MarginLeft = 10,
+        [parameter(Mandatory=$false)][alias("MR")][int]$MarginRight = 10,
+        [parameter(Mandatory=$false)][alias("MT")][int]$MarginTop = 10,
+        [parameter(Mandatory=$false)][alias("MB")][int]$MarginBottom = 10
+    ) 
 
     Begin {
         Add-Type -AssemblyName System.Drawing
@@ -12,19 +23,17 @@ function ConvertTo-Image
     
         foreach ($object in $InputObject) 
         { 
-
             # From http://stackoverflow.com/questions/2067920/can-i-draw-create-an-image-with-a-given-text-with-powershell
             $filename1 = (Join-Path $psScriptRoot (([guid]::NewGuid().ToString())+".png"))
-            #TODO: Configure font name
-            #TODO: Configure font size
-            $font = new-object System.Drawing.Font Consolas,24 
+            #TODO: If text contains newlines (is multi line) then alignment becomes important.
+            $font = new-object System.Drawing.Font $FontName,$FontSize
             $textSize = [System.Windows.Forms.TextRenderer]::MeasureText($object, $font)
             # the size of the image is based on the measure size of the text.
-            $bmp1 = new-object System.Drawing.Bitmap ($textSize.Width+20),($textSize.Height+20)
-            #TODO: Configure background color (including transparency)
-            $brushBg = [System.Drawing.Brushes]::Yellow 
-            #TODO: Configure foreground color (including transparency)
-            $brushFg = [System.Drawing.Brushes]::Black 
+            $bmp1 = new-object System.Drawing.Bitmap ($textSize.Width+$marginLeft+$marginRight),($textSize.Height+$marginTop+$marginBottom)
+            #TODO: Allow for transparent background
+            $brushBg = [System.Drawing.Brushes]::$BackgroundColor 
+            #TODO: Allow for transparent foreground
+            $brushFg = [System.Drawing.Brushes]::$ForegroundColor 
             #TODO: stretch goal... allow for text outline ('stroke')
             #TODO: stretch goal... allow for background to be an image
             $graphics = [System.Drawing.Graphics]::FromImage($bmp1) 
@@ -32,7 +41,7 @@ function ConvertTo-Image
             $graphics.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
             $graphics.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::AntiAlias
             $graphics.FillRectangle($brushBg,0,0,$bmp1.Width,$bmp1.Height) 
-            $graphics.DrawString($object,$font,$brushFg,10,10) 
+            $graphics.DrawString($object,$font,$brushFg,$marginLeft,$marginTop) 
             $graphics.Dispose() 
             $bmp1.Save($filename1) 
             $fileName1;
